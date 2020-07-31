344c5c8f7b2a107362f866a7375c77639d4a75af
Amin <amin@lit.codes>
2020-07-31 12:19:32 +0800

    The complete project

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=marashisamin/prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment --image=$dockerpath prediction
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+podname=$(kubectl get pod -l app=prediction -o name)
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward $podname 8000:80
 
